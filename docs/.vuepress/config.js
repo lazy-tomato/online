5598e78a4d9bdded5a241234178ba0b5c84daffc
tomato <6559210+lazy_tomato@user.noreply.gitee.com>
2022-12-04 19:48:38 +0800

    添加百度站长记录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,6 +26,20 @@ export default defineUserConfig({
         href: '/onlineBlog/images/favicon.ico',
       },
     ],
+    [
+      'script',
+      {},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?338707764965b0734a54d256624e76ed";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+          console.log('132')
+        })();
+        `,
+    ],
   ],
 
   locales: {
