27d3d9f46ec584189d886c6dc71bbe8146357deb
tomato <6559210+lazy_tomato@user.noreply.gitee.com>
2022-12-04 18:57:22 +0800

    学习

---
+++ app/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ this.$route.params }}
+
+    {{ this.$route.query }}
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route.params, '1')
+
+    console.log(this.$route.query, '2')
+  },
+}
+</script>
