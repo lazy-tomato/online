bbc58c19adc1f2052aef19f8a4349187f1f0fe13
tomato <6559210+lazy_tomato@user.noreply.gitee.com>
2022-12-15 09:34:42 +0800

    学习了XMLHttpReques;二分算法的题目

---
+++ 1.js
@@ -0,0 +1,25 @@
+var mySqrt = function (x) {
+  if (x === 0 || x === 1) {
+    return x
+  }
+
+  let left = 1
+  let right = x
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if (mid * mid === x) {
+      return mid
+    }
+    if (mid * mid < x) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+
+  return right
+}
+
+console.log(mySqrt(3))
