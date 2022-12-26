b282828cf984b1d9173d7bf42b98cf45c7a1a546
tomato <6559210+lazy_tomato@user.noreply.gitee.com>
2022-12-26 11:45:18 +0800

    学习几种继承方式

--- 1.js
+++ 1.js
@@ -1,35 +1,22 @@
-Function.prototype.bind2 = function (context) {
-  // 1.新指向的this
-  context = Object(context) || window
-  // 2.调用bind2传入的参数
-  var args = Array.prototype.slice.call(arguments, 1)
-  // 3.调用bind2的函数
-  var self = this
-
-  var temp = function () {}
-
-  // 4. 定义一个函数，当做bind2的返回值
-  var fn = function () {
-    // 5. 返回的函数，获取到的参数
-    var args2 = Array.prototype.slice.call(arguments, 1)
-
-    // self.apply(context, args2.concat(args))
-    // 6. 处理new的情况
-    self.apply(this instanceof temp ? this : context, args2.concat(args))
-  }
-
-  // 7. 处理一下 fn的原型和this的原型
-  temp.prototype = this.prototype
-  fn.prototype = new temp()
-
-  return fn
+function Parent(name) {
+  this.name = name
+  this.hobby = ['lazy', 'tomato']
 }
 
-function swq() {
-  // console.log(1, this.name)
+function Child(name) {
+  Parent.call(this, name)
 }
 
-var f = swq.bind2({ name: 'swq' })
+var child1 = new Child('番茄')
 
-var s = new f()
-// console.log(s)
+var child2 = new Child('测试')
+
+console.log(child1, child2)
+// { name: '番茄', hobby: [ 'lazy', 'tomato' ] }
+// { name: '测试', hobby: [ 'lazy', 'tomato' ] }
+
+child1.hobby.push('番茄2')
+
+console.log(child1, child2)
+// { name: '番茄', hobby: [ 'lazy', 'tomato', '番茄2' ] }
+// { name: '测试', hobby: [ 'lazy', 'tomato' ] }
